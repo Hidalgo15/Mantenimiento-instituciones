9639b431d4c016e94fefb462eed05bd426e45652
Hidalgo15 <emmanuelperezramirez15@gmail.com>
2026-09-21 10:38:08 -0400

    Definicion de Interfaces y ServiciosGenericos

---
+++ ManteniminetoInstituciones/Mantenimiento.Infraestructure/RepositoryServices/GenericRepository/GenericRepository.cs
@@ -0,0 +1,34 @@
+﻿
+using Mantenimiento.Core.Domain.RepositoryInterfaces.GenericRepositoryInterfaces;
+
+namespace Mantenimiento.Infraestructure.Persistence.RepositoryServices.GenericRepository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task<T> AddAsync(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
