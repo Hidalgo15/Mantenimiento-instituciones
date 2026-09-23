1dc646672666669b56f762a598d5b816e7acebfb
IsaacEffect <isaacmartinez9875@gmail.com>
2026-09-23 13:54:26 -0400

    Capitulo hecho

---
+++ ManteniminetoInstituciones/Mantenimiento.Core/Entities/Capitulo.cs
@@ -0,0 +1,9 @@
+﻿namespace Mantenimiento.Core.Domain.Entities
+{
+    public class Capitulo
+    {
+        public int Id { get; set; }
+        public string CodigoCapitulo { get; set; } = string.Empty;
+        public string NombreCapitulo { get; set; } = string.Empty;
+    }
+}
---
+++ ManteniminetoInstituciones/Mantenimiento.Core/Entities/Subcapitulo.cs
@@ -0,0 +1,10 @@
+﻿namespace Mantenimiento.Core.Domain.Entities
+{
+    public class Subcapitulo
+    {
+        public int Id { get; set; }
+        public int IdCapitulo { get; set; }
+        public int CodigoSubcapitulo { get; set; }
+        public string subcapitulo { get; set; } = string.Empty;
+    }
+}
