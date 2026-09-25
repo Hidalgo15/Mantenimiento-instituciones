8184b532f8e9ccef81cbeea34311eb19da2349dd
IsaacEffect <isaacmartinez9875@gmail.com>
2026-09-25 14:30:29 -0400

    agregando daf interface

--- ManteniminetoInstituciones/Mantenimiento.Core/Entities/Daf.cs
+++ ManteniminetoInstituciones/Mantenimiento.Core/Entities/Daf.cs
@@ -2,5 +2,9 @@
 {
     public class Daf
     {
+        public int Id { get; set; }
+        public int IdSubCapitulo { get; set; }
+        public string CodigoDaf { get; set; } = string.Empty;
+        public string NombreDaf { get; set; } = string.Empty;
     }
 }
---
+++ ManteniminetoInstituciones/Mantenimiento.Infraestructure/Repositories/IDafRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mantenimiento.Core.Application.DTOs.Daf;
+
+namespace Mantenimiento.Infraestructure.Persistence.Repositories
+{
+    public interface IDafRepository
+    {
+        Task<IEnumerable<DafDto>> ObtenerAsync(int? id = null, int? idSubCapitulo = null, string? codigoDaf = null);
+    }
+}
